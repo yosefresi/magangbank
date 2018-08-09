692c8f6a138109a1f11b7fe59d1287ab382d758b
Vievin Efendy <vievin.efendy@ti.ukdw.ac.id>
2018-08-09 17:38:35 +0700

    interface mobile apps (login, home, buka rekening)

---
+++ mobile-apps/app/src/main/java/bca/co/id/mini_internet_banking/NewUsernameActivity.java
@@ -0,0 +1,13 @@
+package bca.co.id.mini_internet_banking;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewUsernameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_username);
+    }
+}
